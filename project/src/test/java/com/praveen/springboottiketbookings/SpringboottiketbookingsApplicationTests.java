d6022c9c0470cc7d986fe7983069ec02f7205455
ismail920 <ismail.saleem321@gmail.com>
2024-06-25 11:53:47 +0400

    Update SpringboottiketbookingsApplicationTests.java

--- project/src/test/java/com/praveen/springboottiketbookings/SpringboottiketbookingsApplicationTests.java
+++ project/src/test/java/com/praveen/springboottiketbookings/SpringboottiketbookingsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.praveen.springboottiketbookings;
+package com..springboottiketbookings;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
